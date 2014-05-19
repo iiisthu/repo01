3321f53e87ae150ab472bfcb30688f59617d54d0
freiris <yongxiang@yongxiangs-MacBook-Pro.local>
2014-05-19 17:31:37 +0800

    2nd commit

---
+++ sample01.cpp
@@ -0,0 +1,9 @@
+/* this is a sample code */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello GitHub" << endl;
+	return 0;
+}
---
+++ sample02.py
@@ -0,0 +1,4 @@
+#this is a python sample code
+x = 1
+y = 2
+
